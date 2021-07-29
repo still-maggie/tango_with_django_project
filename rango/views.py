fdda5e1004640eee27eecd525312074c1188921c
still-maggie <mhmz124@163.com>
2021-07-29 23:31:41 +0100

    chapter6_coursework

--- rango/views.py
+++ rango/views.py
@@ -26,12 +26,17 @@ def show_category(request, category_name_slug):
 def index(request):
     # query database by -like and take 0-5
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
 
+    context_dict['pages'] = page_list
+
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(reuqest):
     return render(reuqest, 'rango/about.html', context=None)
+
