be548543e477094d98e1dfb9975154a32aea297c
still-maggie <mhmz124@163.com>
2021-07-30 19:48:20 +0100

    chapter9_coursework

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
+
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
    
     views = models.IntegerField(default=0)
@@ -26,6 +27,7 @@ class Category(models.Model):
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
     URL_MAX_LENGTH = 200
+    
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
--- rango/views.py
+++ rango/views.py
@@ -16,12 +16,11 @@ from rango.forms import PageForm
 
 from rango.forms import UserForm, UserProfileForm
 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login,logout
 
 from django.contrib.auth.decorators import login_required
 
-from datetime import datetime
-
+from django.contrib.auth import authenticate, login, logout
 def show_category(request, category_name_slug):
     context_dict = {}
     try:
@@ -37,11 +36,10 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
         context_dict['pages'] = None
-        
     return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
-    request.session.set_test_cookie()
+    # query database by -like and take 0-5
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -50,26 +48,12 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    visitor_cookie_handler(request)
-
-    context_dict['visits'] = request.session['visits']
-
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(reuqest):
-    
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
+    return render(reuqest, 'rango/about.html', context=None)
 
-    context_dict = {}
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-
-    return render(reuqest, 'rango/about.html', context=context_dict)
-
-
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -86,6 +70,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -168,44 +153,19 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
+# test
 def some_view(request):
     if not request.user.is_authenticated():
         return HttpResponse("You are logged in.")
     else:
         return HttpResponse("You are not logged in.")
 
-
 @login_required
 def restricted(request):
-    return HttpResponse(request, 'rango/restricted.html')
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
-
-
-
-
-
-
-
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        request.session['last_visit'] = last_visit_cookie
-
-    request.session['visits'] = visits
