851fd47eb97f15a993c9d7675a522b267b569852
still-maggie <mhmz124@163.com>
2021-07-30 21:43:37 +0100

    chapter10_coursework_completed

--- rango/views.py
+++ rango/views.py
@@ -46,7 +46,6 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
-    # query database by -like and take 0-5
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -56,12 +55,20 @@ def index(request):
     context_dict['pages'] = page_list
 
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
+    request.session.set_test_cookie()
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
-def about(reuqest):
-    return render(reuqest, 'rango/about.html', context=None)
+def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 @login_required
 def add_category(request):
@@ -163,7 +170,7 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
-# test
+
 def some_view(request):
     if not request.user.is_authenticated():
         return HttpResponse("You are logged in.")
@@ -182,19 +189,22 @@ def user_logout(request):
 
 
 
-def visitor_cookie_handler(request, response):
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-    '%Y-%m-%d %H:%M:%S')
-    
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
     if (datetime.now() - last_visit_time).days > 0:
-    visits = visits + 1
-    
-    response.set_cookie('last_visit', str(datetime.now()))
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
     else:
-    
-    response.set_cookie('last_visit', last_visit_cookie)
-    
-    response.set_cookie('visits', visits)
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
